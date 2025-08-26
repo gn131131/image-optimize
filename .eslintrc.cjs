62faa14d36e4a15d0658b4d493cc6bf97d9b58ed
guannan <guannan@kyland.com>
2025-08-26 10:31:27 +0800

    chore: refactor

--- .eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    root: true,
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint"],
-    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
-    ignorePatterns: ["dist", "node_modules"],
-    rules: { "@typescript-eslint/no-explicit-any": "off" }
-};
--- frontend/src/global.d.ts
+++
@@ -1,3 +0,0 @@
-// allow any for base64 data uri image placeholder
-declare module "*.png";
-declare module "*.jpg";
